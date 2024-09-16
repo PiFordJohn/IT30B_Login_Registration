46a42fcf3df386c0d97be483d43d405eaf1c8eaf
PiFord <cliffordjohnburdeoslutrago@gmail.com>
2024-09-16 20:01:26 +0800

    Added title on login.php container

--- login.php
+++ login.php
@@ -97,6 +97,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 <body>
     <div class="container">
+    <h1>TOURIST REST AREA MANOLO FORTICH</h1>
         <h2>Login</h2>
         <?php if (isset($error)) { echo "<div class='error'>$error</div>"; } ?>
         <form method="POST" action="">
